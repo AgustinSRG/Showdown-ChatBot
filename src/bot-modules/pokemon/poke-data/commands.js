18cd99acee08e78e401d568534c637da1b4e025d
asanrom <agustinsanromanguzman@gmail.com>
2016-06-20 17:51:09 +0200

    Add misc poke-data commands

--- src/bot-modules/pokemon/poke-data/commands.js
+++ src/bot-modules/pokemon/poke-data/commands.js
@@ -4,6 +4,164 @@
 
 'use strict';
 
-module.exports = {
+const Path = require('path');
 
+const getGeneration = require(Path.resolve(__dirname, 'gen.js'));
+const Moves = require(Path.resolve(__dirname, 'moves.js'));
+
+const Text = Tools.get('text.js');
+const Translator = Tools.get('translate.js');
+
+const translator = new Translator(Path.resolve(__dirname, 'commands.translations'));
+
+App.parser.addPermission('pokemon', {group: 'voice'});
+
+module.exports = {
+	gen: function () {
+		let id = Text.toId(this.arg);
+		if (!id) return this.errorReply(this.usage({desc: 'pokemon/item/ability/move'}));
+		let gen;
+		try {
+			let aliases = App.data.getAliases();
+			if (aliases[id]) id = Text.toId(aliases[id]);
+			gen = getGeneration(id);
+		} catch (err) {
+			App.reportCrash(err);
+			return this.errorReply(translator.get('error', this.lang));
+		}
+		let text = '';
+		switch (gen.gen) {
+		case 'metronome':
+			text = translator.get(0, this.lang);
+			break;
+		case 0:
+			text = translator.get(1, this.lang) + " __" + id + "__ " + translator.get(2, this.lang);
+			break;
+		default:
+			text = translator.get(3, this.lang) + " __" + gen.name + "__: " + translator.get(4, this.lang) + " " + gen.gen;
+		}
+		this.restrictReply(text, 'pokemon');
+	},
+
+	viablemoves: 'randommoves',
+	randommoves: function () {
+		if (!this.arg) return this.errorReply(this.usage({desc: 'pokemon'}, {desc: 'singles/doubles', optional: true}));
+		let id = Text.toId(this.args[0]);
+		let type = Text.toId(this.args[1]);
+		if (!id) return this.errorReply(this.usage({desc: 'pokemon'}, {desc: 'singles/doubles', optional: true}));
+		let doubles;
+		if (type in {'doubles': 1, '2': 1, 'double': 1, 'triples': 1, 'triple': 1, '3': 1}) {
+			type = 'randomDoubleBattleMoves';
+			doubles = true;
+		} else {
+			type = 'randomBattleMoves';
+			doubles = false;
+		}
+		let moves;
+		try {
+			let aliases = App.data.getAliases();
+			if (aliases[id]) id = Text.toId(aliases[id]);
+			moves = Moves.getRandomBattleMoves(id, type);
+		} catch (err) {
+			App.reportCrash(err);
+			return this.errorReply(translator.get('error', this.lang));
+		}
+		if (moves === null) {
+			return this.errorReply(translator.get(5, this.lang) + ' __' + id + '__ ' + translator.get(2, this.lang));
+		} else if (!moves.length) {
+			return this.errorReply(translator.get(6, this.lang) + ' __' + Moves.getPokeName(id) + '__');
+		} else if (doubles) {
+			return this.restrictReply(translator.get(7, this.lang) + ' __' + Moves.getPokeName(id) + '__: ' + moves.join(', '), 'pokemon');
+		} else {
+			return this.restrictReply(translator.get(8, this.lang) + ' __' + Moves.getPokeName(id) + '__: ' + moves.join(', '), 'pokemon');
+		}
+	},
+
+	priority: function () {
+		if (!this.arg) return this.errorReply(this.usage({desc: 'pokemon'}));
+		let id = Text.toId(this.args[0]);
+		if (!id) return this.errorReply(this.usage({desc: 'pokemon'}));
+		let moves;
+		try {
+			let aliases = App.data.getAliases();
+			if (aliases[id]) id = Text.toId(aliases[id]);
+			moves = Moves.getPriorityMoves(id);
+		} catch (err) {
+			App.reportCrash(err);
+			return this.errorReply(translator.get('error', this.lang));
+		}
+		if (moves === null) {
+			return this.errorReply(translator.get(5, this.lang) + ' __' + id + '__ ' + translator.get(2, this.lang));
+		} else if (!moves.length) {
+			return this.errorReply(translator.get(9, this.lang) + ' __' + Moves.getPokeName(id) + '__');
+		} else {
+			return this.restrictReply(translator.get(10, this.lang) + ' __' + Moves.getPokeName(id) + '__: ' + moves.join(', '), 'pokemon');
+		}
+	},
+
+	boosting: function () {
+		if (!this.arg) return this.errorReply(this.usage({desc: 'pokemon'}));
+		let id = Text.toId(this.args[0]);
+		if (!id) return this.errorReply(this.usage({desc: 'pokemon'}));
+		let moves;
+		try {
+			let aliases = App.data.getAliases();
+			if (aliases[id]) id = Text.toId(aliases[id]);
+			moves = Moves.getBoostingMoves(id);
+		} catch (err) {
+			App.reportCrash(err);
+			return this.errorReply(translator.get('error', this.lang));
+		}
+		if (moves === null) {
+			return this.errorReply(translator.get(5, this.lang) + ' __' + id + '__ ' + translator.get(2, this.lang));
+		} else if (!moves.length) {
+			return this.errorReply(translator.get(11, this.lang) + ' __' + Moves.getPokeName(id) + '__');
+		} else {
+			return this.restrictReply(translator.get(12, this.lang) + ' __' + Moves.getPokeName(id) + '__: ' + moves.join(', '), 'pokemon');
+		}
+	},
+
+	recovery: function () {
+		if (!this.arg) return this.errorReply(this.usage({desc: 'pokemon'}));
+		let id = Text.toId(this.args[0]);
+		if (!id) return this.errorReply(this.usage({desc: 'pokemon'}));
+		let moves;
+		try {
+			let aliases = App.data.getAliases();
+			if (aliases[id]) id = Text.toId(aliases[id]);
+			moves = Moves.getRecoveryMoves(id);
+		} catch (err) {
+			App.reportCrash(err);
+			return this.errorReply(translator.get('error', this.lang));
+		}
+		if (moves === null) {
+			return this.errorReply(translator.get(5, this.lang) + ' __' + id + '__ ' + translator.get(2, this.lang));
+		} else if (!moves.length) {
+			return this.errorReply(translator.get(13, this.lang) + ' __' + Moves.getPokeName(id) + '__');
+		} else {
+			return this.restrictReply(translator.get(14, this.lang) + ' __' + Moves.getPokeName(id) + '__: ' + moves.join(', '), 'pokemon');
+		}
+	},
+
+	hazards: function () {
+		if (!this.arg) return this.errorReply(this.usage({desc: 'pokemon'}));
+		let id = Text.toId(this.args[0]);
+		if (!id) return this.errorReply(this.usage({desc: 'pokemon'}));
+		let moves;
+		try {
+			let aliases = App.data.getAliases();
+			if (aliases[id]) id = Text.toId(aliases[id]);
+			moves = Moves.getHazardsMoves(id);
+		} catch (err) {
+			App.reportCrash(err);
+			return this.errorReply(translator.get('error', this.lang));
+		}
+		if (moves === null) {
+			return this.errorReply(translator.get(5, this.lang) + ' __' + id + '__ ' + translator.get(2, this.lang));
+		} else if (!moves.length) {
+			return this.errorReply(translator.get(15, this.lang) + ' __' + Moves.getPokeName(id) + '__');
+		} else {
+			return this.restrictReply(translator.get(16, this.lang) + ' __' + Moves.getPokeName(id) + '__: ' + moves.join(', '), 'pokemon');
+		}
+	},
 };
---
+++ src/bot-modules/pokemon/poke-data/gen.js
@@ -0,0 +1,46 @@
+/**
+ * Gets the generation of a Pokemon, item, move or ability
+ */
+
+'use strict';
+
+const Text = Tools.get('text.js');
+
+function getGeneration(name) {
+	let id = Text.toId(name);
+	let pokedex = App.data.getPokedex();
+	let movedex = App.data.getMoves();
+	let items = App.data.getItems();
+	let abilities = App.data.getAbilities();
+	if (id === 'metronome') {
+		return {gen: 'metronome'};
+	} else if (pokedex[id]) {
+		if (pokedex[id].num < 0) return {gen: 'CAP', name: pokedex[id].species};
+		else if (pokedex[id].num <= 151) return {gen: 1, name: pokedex[id].species};
+		else if (pokedex[id].num <= 251) return {gen: 2, name: pokedex[id].species};
+		else if (pokedex[id].num <= 386) return {gen: 3, name: pokedex[id].species};
+		else if (pokedex[id].num <= 493) return {gen: 4, name: pokedex[id].species};
+		else if (pokedex[id].num <= 649) return {gen: 5, name: pokedex[id].species};
+		else return {gen: 6, name: pokedex[id].species};
+	} else if (movedex[id]) {
+		if (movedex[id].num <= 165) return {gen: 1, name: movedex[id].name};
+		else if (movedex[id].num <= 251) return {gen: 2, name: movedex[id].name};
+		else if (movedex[id].num <= 354) return {gen: 3, name: movedex[id].name};
+		else if (movedex[id].num <= 467) return {gen: 4, name: movedex[id].name};
+		else if (movedex[id].num <= 559) return {gen: 5, name: movedex[id].name};
+		else if (movedex[id].num <= 617) return {gen: 6, name: movedex[id].name};
+		else return {gen: 'CAP', name: movedex[id].name};
+	} else if (abilities[id]) {
+		if (abilities[id].num <= 0) return {gen: 'CAP', name: abilities[id].name};
+		else if (abilities[id].num <= 76) return {gen: 3, name: abilities[id].name};
+		else if (abilities[id].num <= 123) return {gen: 4, name: abilities[id].name};
+		else if (abilities[id].num <= 164) return {gen: 5, name: abilities[id].name};
+		else return {gen: 6, name: abilities[id].name};
+	} else if (items[id]) {
+		return {gen: items[id].gen, name: items[id].name};
+	} else {
+		return {gen: 0};
+	}
+}
+
+module.exports = getGeneration;
