d6b56a45d0ca05beae1f9f4c4439b2abfd9c2c0e
asanrom <agustinsanromanguzman@gmail.com>
2016-06-28 20:39:56 +0200

    Add 'reload data' option to hotpatch menu

--- src/server/handlers/hotpatch.js
+++ src/server/handlers/hotpatch.js
@@ -30,6 +30,10 @@ App.server.setHandler('hotpatch', (context, parts) => {
 		} catch (err) {
 			error = "Error: " + err.code + " - " + err.message;
 		}
+	} else if (context.post.reloaddata) {
+		App.data.downloadAll();
+		App.logServerAction(context.user.id, 'Reload Data.');
+		ok = "Downloading data. It will be realoaded in a few seconds.";
 	}
 
 	/* Generate HTML */
@@ -40,11 +44,19 @@ App.server.setHandler('hotpatch', (context, parts) => {
 	html += '<p><label><input type="submit" name="hotpatch" value="Hotpatch Commands" /></label></p>';
 	html += '</form>';
 
+	html += '<hr />';
+
+	html += '<h2>Reload Data</h2>';
+	html += '<p>Data is reloaded every time the bot connects to the server, but you can manually reaload it with this option.</p>';
+	html += '<form method="post" action="">';
+	html += '<p><label><input type="submit" name="reloaddata" value="Reload Data" /></label></p>';
+	html += '</form>';
+
 	if (error) {
 		html += '<p style="padding:5px;"><span class="error-msg">' + error + '</span></p>';
 	} else if (ok) {
 		html += '<p style="padding:5px;"><span class="ok-msg">' + ok + '</span></p>';
 	}
 
-	context.endWithWebPage(html, {title: "Hotpatch Commands - Showdown ChatBot"});
+	context.endWithWebPage(html, {title: "Hotpatch - Showdown ChatBot"});
 });
