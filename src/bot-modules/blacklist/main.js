22c2ebb3b8df9dfa8a4f191e047f6c23b163a813
asanrom <agustinsanromanguzman@gmail.com>
2016-07-02 18:18:37 +0200

    Only send ban commands if the bot can do it

--- src/bot-modules/blacklist/main.js
+++ src/bot-modules/blacklist/main.js
@@ -20,6 +20,12 @@ function getLanguage(room) {
 	return App.config.language.rooms[room] || App.config.language['default'];
 }
 
+function botCanBan(room) {
+	let roomData = App.bot.rooms[room];
+	let botid = Text.toId(App.bot.getBotNick());
+	return (roomData && roomData.users[botid] && App.parser.equalOrHigherGroup({group: roomData.users[botid]}, 'mod'));
+}
+
 exports.blacklist = function (room, user) {
 	user = Text.toId(user);
 	if (!user) return false;
@@ -53,6 +59,7 @@ exports.getInitCmds = function () {
 		for (let id in users) {
 			let group = users[id];
 			if (App.parser.equalOrHigherGroup({group: group}, 'driver')) continue; // Do not ban staff
+			if (!botCanBan(room)) continue; // Bot cannot ban
 			if (data[room] && data[room][id]) {
 				cmds.push(room + '|/roomban ' + id + ', ' + translator.get('ban', getLanguage(room)));
 			}
@@ -64,6 +71,7 @@ exports.getInitCmds = function () {
 App.bot.on('userjoin', (room, by) => {
 	let user = Text.parseUserIdent(by);
 	if (App.parser.equalOrHigherGroup(user, 'driver')) return; // Do not ban staff
+	if (!botCanBan(room)) return; // Bot cannot ban
 	if (data[room] && data[room][user.id]) {
 		App.bot.sendTo(room, '/roomban ' + user.id + ', ' + translator.get('ban', getLanguage(room)));
 	}
@@ -72,6 +80,7 @@ App.bot.on('userjoin', (room, by) => {
 App.bot.on('userrename', (room, old, by) => {
 	let user = Text.parseUserIdent(by);
 	if (App.parser.equalOrHigherGroup(user, 'driver')) return; // Do not ban staff
+	if (!botCanBan(room)) return; // Bot cannot ban
 	if (data[room] && data[room][user.id]) {
 		App.bot.sendTo(room, '/roomban ' + user.id + ', ' + translator.get('ban', getLanguage(room)));
 	}
