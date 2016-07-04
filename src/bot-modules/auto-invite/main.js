6094d3ba6e4bc783793d9b011dc6d61d6634e831
asanrom <agustinsanromanguzman@gmail.com>
2016-07-04 15:31:02 +0200

    Add module: Auto-Invite

---
+++ src/bot-modules/auto-invite/main.js
@@ -0,0 +1,119 @@
+/*
+ * Bot Module: AutoInvite
+ */
+
+'use strict';
+
+const Roomauth_Check_Interval = 30 * 1000;
+
+const Path = require('path');
+
+const Text = Tools.get('text.js');
+
+if (!App.config.modules.autoinvite) {
+	App.config.modules.autoinvite = {
+		room: '',
+		rank: 'voice',
+	};
+}
+
+exports.roomAuth = {};
+exports.roomAuthChanged = false;
+exports.timer = null;
+
+exports.parsePopup = function (popup) {
+	if (Text.toId(popup.substr(0, 4)) === 'room') {
+		let auth = {};
+		let _message = popup.replace("Moderators", ":");
+		_message = _message.replace("Drivers", ":");
+		_message = _message.replace("Voices", ":");
+		let parts = _message.split(':');
+		let rank = " ";
+		let usersList;
+		for (let i = 0; i < parts.length; i += 2) {
+			usersList = parts[i + 1].split(',');
+			if (parts[i].indexOf("(") > -1) rank = parts[i].substr(parts[i].indexOf("(") + 1, 1);
+			for (let f = 0; f < usersList.length; f++) {
+				auth[Text.toId(usersList[f])] = rank;
+			}
+		}
+		exports.roomAuth = auth;
+		App.bot.removeListener('popup', exports.parsePopup);
+	}
+};
+
+exports.getInitCmds = function () {
+	let cmds = [];
+	if (App.config.modules.autoinvite.room) {
+		cmds.push(App.config.modules.autoinvite.room + '|/roomauth');
+		App.bot.on('popup', exports.parsePopup);
+	}
+	return cmds;
+};
+
+exports.checkRoomAuth = function () {
+	if (exports.roomAuthChanged) {
+		exports.roomAuthChanged = false;
+		let cmds = exports.getInitCmds();
+		if (cmds.length) {
+			App.bot.send(cmds);
+		}
+	}
+};
+
+exports.userCanBeInvited = function (room, ident) {
+	let tarRoom = App.config.modules.autoinvite.room;
+	if (!tarRoom) return false;
+	if (tarRoom === room) return false;
+	if (App.bot.rooms[tarRoom] && App.bot.rooms[tarRoom].users[ident.id]) return false;
+	let rank = exports.roomAuth[ident.id];
+	let tarRank = App.config.modules.autoinvite.rank;
+	if (App.parser.equalOrHigherGroup({group: rank}, tarRank)) {
+		return true;
+	} else {
+		return false;
+	}
+};
+
+App.bot.on('userjoin', (room, by) => {
+	let user = Text.parseUserIdent(by);
+	if (exports.userCanBeInvited(room, user)) {
+		App.bot.sendTo('', '/invite ' + user.id + ', ' + App.config.modules.autoinvite.room);
+	}
+});
+
+App.bot.on('userrename', (room, old, by) => {
+	let user = Text.parseUserIdent(by);
+	if (user.id === Text.toId(old)) return;
+	if (exports.userCanBeInvited(room, user)) {
+		App.bot.sendTo('', '/invite ' + user.id + ', ' + App.config.modules.autoinvite.room);
+	}
+});
+
+App.bot.on('line', (room, line, spl, isIntro) => {
+	if (isIntro) return;
+	if (!App.bot.rooms[room] || App.bot.rooms[room].type !== "chat") return;
+	if (!App.config.modules.autoinvite.room || App.config.modules.autoinvite.room !== room) return;
+	if (line.substr(0) !== '|') {
+		if (line.indexOf(" was promoted ") >= 0 || line.indexOf(" was demoted ") >= 0) {
+			exports.roomAuthChanged = true;
+		}
+	}
+});
+
+App.bot.on('connect', () => {
+	if (exports.timer) {
+		clearInterval(exports.timer);
+		exports.timer = null;
+	}
+	exports.timer = setInterval(exports.checkRoomAuth, Roomauth_Check_Interval);
+});
+
+App.bot.on('disconnect', () => {
+	if (exports.timer) {
+		clearInterval(exports.timer);
+		exports.timer = null;
+	}
+});
+
+require(Path.resolve(__dirname, 'server-handler.js'));
---
+++ src/bot-modules/auto-invite/server-handler.js
@@ -0,0 +1,85 @@
+/**
+ * Server Handler: Auto-Invite
+ */
+
+'use strict';
+
+const Text = Tools.get('text.js');
+
+/* Permissions */
+
+App.server.setPermission('autoinvite', 'Permission for changing the auto-invite configuration');
+
+/* Menu Options */
+
+App.server.setMenuOption('autoinvite', 'Auto-Invite', '/autoinvite/', 'autoinvite');
+
+/* Handlers */
+
+App.server.setHandler('autoinvite', (context, parts) => {
+	/* Permission check */
+	if (!context.user || !context.user.can('autoinvite')) {
+		context.endWith403();
+		return;
+	}
+
+	/* Actions */
+	let ok = null, error = null;
+
+	if (context.post.edit) {
+		let room = Text.toRoomid(context.post.room);
+		let rank = context.post.rank || "excepted";
+		if (rank.length > 1 && App.config.parser[rank]) {
+			rank = App.config.parser[rank];
+		}
+		if (rank === 'user' || rank === 'excepted' || App.config.parser.groups.indexOf(rank) >= 0) {
+			App.config.modules.autoinvite.room = room;
+			App.config.modules.autoinvite.rank = rank;
+			App.db.write();
+			App.modules.autoinvite.system.roomAuth = {};
+			App.modules.autoinvite.system.roomAuthChanged = true;
+			App.logServerAction(context.user.id, "Edit autoinvite configuration");
+			ok = "Auto-Invite configuration saved";
+		} else {
+			error = "Invalid Rank";
+		}
+	}
+
+	/* Generate Html */
+	let html = '';
+	html += '<h2>Auto-Invite Configuration</h2>';
+	html += '<form method="post" action="">';
+	html += '<table border="0">';
+	html += '<tr><td><strong>Room</strong>:&nbsp;</td><td><input name="room" type="text" size="40" value="' +
+		(App.config.modules.autoinvite.room || "") + '" /></td></tr>';
+	html += '<tr><td><strong>Rank</strong>:&nbsp;</td><td>' +
+		getRankSelect('rank', App.config.modules.autoinvite.rank) + '</td></tr>';
+	html += '</table>';
+	html += '<p><input type="submit" name="edit" value="Save Changes" /></p>';
+	html += '</form>';
+
+	if (error) {
+		html += '<p style="padding:5px;"><span class="error-msg">' + error + '</span></p>';
+	} else if (ok) {
+		html += '<p style="padding:5px;"><span class="ok-msg">' + ok + '</span></p>';
+	}
+
+	context.endWithWebPage(html, {title: "AutoInvite - Showdown ChatBot"});
+});
+
+function getRankSelect(name, rank) {
+	if (!rank) rank = '';
+	if (rank.length > 1 && App.config.parser[rank]) {
+		rank = App.config.parser[rank];
+	}
+	let html = '';
+	html += '<select name="' + name + '">';
+	html += '<option value="user"' + (rank === 'user' ? ' selected="selected"' : '') + '>Regular Users</option>';
+	html += '<option value="excepted"' + (rank === 'excepted' ? ' selected="selected"' : '') + '>Excepted Users</option>';
+	for (let j = 0; j < App.config.parser.groups.length; j++) {
+		html += '<option value="' + App.config.parser.groups[j] + '"' +
+			(rank === App.config.parser.groups[j] ? ' selected="selected"' : '') + '>Group ' + App.config.parser.groups[j] + '</option>';
+	}
+	html += '</select>';
+	return html;
+}
