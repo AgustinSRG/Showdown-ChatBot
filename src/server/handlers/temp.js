7c4038407291e1e6a2eeb8ecdaa3f11ab5b0db25
asanrom <agustinsanromanguzman@gmail.com>
2016-07-07 21:46:14 +0200

    Fix bug in 'temp' server handler

--- src/server/handlers/temp.js
+++ src/server/handlers/temp.js
@@ -8,7 +8,7 @@ const Path = require('path');
 
 App.server.setHandler('temp', (context, parts) => {
 	if (parts.length && parts[0]) {
-		let file = Path.resolve(__dirname, '../../../data/temp/', parts.shift().split('?')[0] + '.tmp');
+		let file = Path.resolve(App.dataDir, 'temp', parts.shift().split('?')[0] + '.tmp');
 		context.endWithStaticFile(file);
 	} else {
 		context.endWithError('403', 'Forbidden', 'You have not permission to access this path!');
