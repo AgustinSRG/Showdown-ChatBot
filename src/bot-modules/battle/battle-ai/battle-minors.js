9b6742331c18030dd547f0a5520f5d7c2a5a38b2
asanrom <agustinsanromanguzman@gmail.com>
2016-07-07 20:09:02 +0200

    Battle: Report when losing due to inactivity

--- src/bot-modules/battle/battle-ai/battle-minors.js
+++ src/bot-modules/battle/battle-ai/battle-minors.js
@@ -616,6 +616,9 @@ module.exports = {
 		if (args[1] && args[1].indexOf("NOTE: This is an Inverse Battle") >= 0) {
 			this.conditions["inversebattle"] = true;
 			this.debug(this.id + ": Changed to inverse Battle");
+		} else if (args[1] && args[1].trim() === (App.bot.getBotNick() + ' lost due to inactivity.')) {
+			App.log("Bot lost due to inactivity. Battle system error. Room: " +
+				this.id + " | " + JSON.stringify(this.lastSend));
 		}
 	},
 };
