e316a5e0ad43b19afea2a143a8eacaebac1f5508
asanrom <agustinsanromanguzman@gmail.com>
2016-08-30 20:05:41 +0200

    Remove sugar dependency

--- src/dependencies.js
+++ src/dependencies.js
@@ -6,7 +6,6 @@
 'use strict';
 
 try {
-	require('sugar');
 	require('websocket');
 	require('githubhook');
 } catch (e) {
@@ -14,4 +13,19 @@ try {
 	require('child_process').spawnSync('sh', ['-c', 'npm install --production'], {stdio: 'inherit'});
 }
 
-require('sugar');
+if (!Object.merge) {
+	Object.merge = function (object, source) {
+		if (!object) object = {};
+		if (!source) return object;
+		for (let key in source) object[key] = source[key];
+		return object;
+	};
+}
+
+if (!Object.values) {
+	Object.values = function (object) {
+		let values = [];
+		for (let key in object) values.push(object[key]);
+		return values;
+	};
+}
