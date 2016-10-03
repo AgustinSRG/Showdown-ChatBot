c8f18993bede24476932beef2c172b86eb276f01
asanrom <agustinsanromanguzman@gmail.com>
2016-10-03 22:31:04 +0200

    Tour-Cmd: Handle errors from the tournament system

--- src/bot-modules/tour-cmd/main.js
+++ src/bot-modules/tour-cmd/main.js
@@ -5,6 +5,9 @@
 'use strict';
 
 const Path = require('path');
+const Translator = Tools.get('translate.js');
+
+const translator = new Translator(Path.resolve(__dirname, 'errors.translations'));
 
 const Tournament = require(Path.resolve(__dirname, 'tournament.js'));
 
@@ -33,9 +36,38 @@ exports.newTour = function (room, details) {
 	tournaments[room].create();
 };
 
+const Error_Open = '<div class="message-error">';
+const Error_Close = '</div>';
+
+function getLanguage(room) {
+	return App.config.language.rooms[room] || App.config.language['default'];
+}
+
+function parseErrorMessage(room, spl) {
+	if (!tournaments[room] || tourData[room]) return;
+	let msg = spl.splice(1).join('|');
+	if (msg.substr(0, Error_Open.length) === Error_Open) {
+		msg = msg.substr(Error_Open.length, msg.length - (Error_Open.length + Error_Close.length));
+		/* Specific error messages, may be updated frecuently */
+		if (msg === "Tournaments are disabled in this room (" + room + ").") {
+			App.bot.sendTo(room, translator.get(0, getLanguage(room)));
+		} else if (msg === "&#x2f;tournament - Access denied.") {
+			App.bot.sendTo(room, translator.get(1, getLanguage(room)));
+		} else if (msg === "The server is restarting soon, so a tournament cannot be created.") {
+			App.bot.sendTo(room, translator.get(2, getLanguage(room)));
+		} else {
+			return;
+		}
+		if (tournaments[room].startTimer) clearTimeout(tournaments[room].startTimer);
+		delete tournaments[room];
+		delete tourData[room];
+	}
+}
+
 App.bot.on('line', (room, line, spl, isIntro) => {
-	if (spl[0] !== 'tournament') return;
 	if (isIntro) return;
+	if (spl[0] === 'html') return parseErrorMessage(room, spl);
+	if (spl[0] !== 'tournament') return;
 	if (!tourData[room]) tourData[room] = {};
 	switch (spl[1]) {
 	case 'create':
