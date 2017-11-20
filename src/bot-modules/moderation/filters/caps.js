84515d2120c760629f725299ecd2f9f2c74432bd
Agustin San Roman <agustinsanromanguzman@gmail.com>
2017-11-20 09:55:02 +0100

    Moderation: Improve caps filter

--- src/bot-modules/moderation/filters/caps.js
+++ src/bot-modules/moderation/filters/caps.js
@@ -9,7 +9,6 @@ const Min_Caps_Proportion = 0.8;
 const Caps_Default_Value = 1;
 
 const Path = require('path');
-const Text = Tools('text');
 
 const Lang_File = Path.resolve(__dirname, 'caps.translations');
 
@@ -17,11 +16,11 @@ exports.id = 'caps';
 
 exports.parse = function (context) {
 	let msg = context.noNicksMsg;
-	let capsMatch = msg.replace(/[^A-Za-z]/g, '').match(/[A-Z]/g);
-	capsMatch = capsMatch && Text.toId(msg).length > Min_Caps_Length &&
-		(capsMatch.length >= Math.floor(Text.toId(msg).length * Min_Caps_Proportion));
-	let val = this.getModTypeValue(exports.id, Caps_Default_Value);
-	if (capsMatch) {
+	let capsMatch = msg.match(/[A-Z]/g);
+	capsMatch = capsMatch && msg.length > Min_Caps_Length &&
+		(capsMatch.length >= Math.floor(msg.length * Min_Caps_Proportion));
+	if (capsMatch || msg.match(/[A-Z\s]{18,}/)) {
+		let val = this.getModTypeValue(exports.id, Caps_Default_Value);
 		context.infractions.push(exports.id);
 		context.totalPointVal += val;
 		if (context.pointVal < val) {
