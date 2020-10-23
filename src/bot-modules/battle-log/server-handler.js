1e2cf79b53cfaa321f375a48f8f01b1732b64f36
AgustinSRG <41982624+AgustinSRG@users.noreply.github.com>
2020-10-23 18:09:32 +0200

    Fix build

--- src/bot-modules/battle-log/server-handler.js
+++ src/bot-modules/battle-log/server-handler.js
@@ -66,16 +66,16 @@ exports.setup = function (App) {
 			htmlVars.log_files += '<tr>';
 			htmlVars.log_files += '<td>' + logs[i].file + '</td>';
 			htmlVars.log_files += '<td style="text-align: center; white-space: nowrap;"><a href="/battlelog/' + logs[i].file +
-			'" target="_blank" rel="noopener noreferrer"><button>View Log</button></a>&nbsp;|&nbsp;<a href="' + logs[i].psim +
-			'" target="_blank" rel="noopener noreferrer"><button>Recreate battle</button></a></td>';
+				'" target="_blank" rel="noopener noreferrer"><button>View Log</button></a>&nbsp;|&nbsp;<a href="' + logs[i].psim +
+				'" target="_blank" rel="noopener noreferrer"><button>Recreate battle</button></a></td>';
 			htmlVars.log_files += '<td>' + Text.escapeHTML(logs[i].title) + '</td>';
 			htmlVars.log_files += '<td>' + logs[i].date + '</td>';
 			htmlVars.log_files += '<td>' + logs[i].size + ' KB</td>';
 			htmlVars.log_files += '<td>' + Text.escapeHTML(logs[i].state || "-") + '</td>';
-			
+
 			htmlVars.log_files += '</tr>';
 		}
 
-		context.endWithWebPage(mainTemplate.make(htmlVars), {title: "Battle Log - Showdown ChatBot"});
+		context.endWithWebPage(mainTemplate.make(htmlVars), { title: "Battle Log - Showdown ChatBot" });
 	});
 };
