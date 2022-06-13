0bfa33ad07b10ae72475e3d343862eee4076ad20
AgustinSRG <41982624+AgustinSRG@users.noreply.github.com>
2022-06-13 12:18:44 +0200

    Remove uncachetree

--- src/tools/require-eval.js
+++ src/tools/require-eval.js
@@ -4,17 +4,10 @@
 
 'use strict';
 
-const Path = require('path');
-const FileSystem = require('fs');
-const uncacheTree = Tools('uncachetree');
+const ModuleFromString = require("module-from-string");
 
 function run(App, script) {
-	let path = Path.resolve(App.dataDir, 'tmp-require.js');
-	try {
-		uncacheTree(path);
-	} catch (e) {}
-	FileSystem.writeFileSync(path, script);
-	return require(path);
+	return ModuleFromString.importFromStringSync(script, { globals: global });
 }
 
 exports.run = run;
--- src/tools/uncachetree.js
+++ src/tools/uncachetree.js
@@ -9,19 +9,5 @@
  * @param {Path} root
  */
 module.exports = function (root) {
-	let uncache = [require.resolve(root)];
-	do {
-		let newuncache = [];
-		for (let i = 0; i < uncache.length; ++i) {
-			if (require.cache[uncache[i]]) {
-				newuncache.push.apply(newuncache,
-					require.cache[uncache[i]].children.map(module => {
-						return module.filename;
-					})
-				);
-				delete require.cache[uncache[i]];
-			}
-		}
-		uncache = newuncache;
-	} while (uncache.length > 0);
+	return; // Deprecated
 };
